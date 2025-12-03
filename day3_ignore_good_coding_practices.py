04d46b16aed36052722cefeaf824270019cef86d
DaveMikeH8192 <david.howard@datasite.com>
2025-12-03 19:19:53 +0000

    update of code

--- day3_ignore_good_coding_practices.py
+++ day3_ignore_good_coding_practices.py
@@ -4,8 +4,8 @@ for z in 1,11:
  for l in sys.argv[1].split("\n"):
   s=""
   for i in range(z,-1,-1):
-   f,c,u,q=l[0],1,l[1:-i] if i!=0 else l[1:],0
-   for j in u:
+   f,c,q=l[0],1,0
+   for j in l[1:-i] if i!=0 else l[1:]:
     f,q=(j,c) if j>f else (f,q)
     c+=1
    s,l=s+f,l[q+1:]
